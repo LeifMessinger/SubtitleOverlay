7a3039f36f347861d204a6140eceb2bac958d8f4
Leif Messinger <leifmessinger@gmail.com>
2024-02-14 18:30:25 -0600

    Added broken outline shader

--- game/src/subtitles.c
+++ game/src/subtitles.c
@@ -4,12 +4,23 @@
 RenderTexture2D target;
 Vector2 targetDimensions = {1, 1};
 Font subtitleFont;
+Shader outlineShader;
 const int SUBTITLE_FONT_SIZE = 100;
 
+#if defined(PLATFORM_DESKTOP)
+    #define GLSL_VERSION            330
+#else   // PLATFORM_ANDROID, PLATFORM_WEB
+    #define GLSL_VERSION            100
+#endif
+
 void LoadSubtitles(){
-	subtitleFont = LoadFontEx("resources/RoadgeekMittelschrift.ttf", SUBTITLE_FONT_SIZE, NULL, 0);
+	subtitleFont = LoadFontEx("resources/fonts/RoadgeekMittelschrift.ttf", SUBTITLE_FONT_SIZE, NULL, 0);
 	
 	target = LoadRenderTexture(1, 1);
+	
+	if(OUTLINE){
+		outlineShader = LoadShader(0, TextFormat("resources/shaders/glsl%i/outline.fs", GLSL_VERSION));
+	}
 
 	//printf("Screen: (%d, %d)\t Monitor: (%d, %d)\n", GetScreenWidth(), GetScreenHeight(), GetMonitorWidth(0), GetMonitorHeight(0));
 }
@@ -27,7 +38,11 @@ void UpdateSubtitleTexture(const char* subtitleText){
 	target = LoadRenderTexture(targetDimensions.x, targetDimensions.y);
 	//Drawing to the texture
 	BeginTextureMode(target);
-	ClearBackground(subtitleBoxColor);
+	if(BACKGROUND){
+		ClearBackground(subtitleBoxColor);
+	}else{
+		ClearBackground(BLANK);	//Still need to clear it to get rid of anything
+	}
 	DrawTextEx(subtitleFont, subtitleText, (Vector2){subtitleBoundingBoxExtra.x / 2, subtitleBoundingBoxExtra.y / 2}, SUBTITLE_FONT_SIZE, 5, subtitleTextColor);
 	EndTextureMode();
 }
@@ -56,6 +71,17 @@ void DrawSubtitleTexture(){
 	const Vector2 destinationSize = {targetDimensions.x * scale, targetDimensions.y * scale};
 	const Vector2 subtitlePosition = {center.x, GetScreenHeight() - (destinationSize.y * 1.5)};	//Center of the subtitles
 	const Rectangle subtitleDestination = rectangleFromSizeCenteredAroundPosition(destinationSize, subtitlePosition);
+	if(OUTLINE){
+		//We put this in the draw loop so the amount of border can change while running.
+		const float tScale[2] = { (float)destinationSize.x, (float)destinationSize.y };
+		SetShaderValue(outlineShader, GetShaderLocation(outlineShader, "textureSize"), tScale, SHADER_UNIFORM_VEC2);
+		const float outlineSize = 10.0f;
+		SetShaderValue(outlineShader, GetShaderLocation(outlineShader, "outlineSize"), &outlineSize, SHADER_UNIFORM_FLOAT);
+		const float outlineColor[4] = {0.0f, 0.0f, 0.0f, 1.0f};	//Hoping that 1 is 100% opacity
+		SetShaderValue(outlineShader, GetShaderLocation(outlineShader, "outlineColor"), outlineColor, SHADER_UNIFORM_VEC4);
+		
+		BeginShaderMode(outlineShader);
+	}
 	DrawTexturePro(
 		target.texture,
 		(Rectangle){0, 0, targetDimensions.x, -targetDimensions.y},
@@ -64,6 +90,9 @@ void DrawSubtitleTexture(){
 		0, //Rotation
 		WHITE	//Color
 	);
+	if(OUTLINE){
+		EndShaderMode();
+	}
 	
 	if(IsCursorOnScreen()){
 		if(pointIsInRectangle(GetMousePosition(), subtitleDestination)){
@@ -79,4 +108,8 @@ void DrawSubtitleTexture(){
 void UnloadSubtitles(){
 	UnloadFont(subtitleFont);
 	UnloadRenderTexture(target);
+	
+	if(OUTLINE){
+		UnloadShader(outlineShader);
+	}
 }
